078ad79c0b16cea34de723507ee146e75192bd22
Daniel Anderson <danandwords@outlook.com>
2024-12-10 13:39:38 +0000

    fix issue with blog styling not showing and summernote text box not appearing

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 
 # Create your views here.
 
+
 class PostList(generic.ListView):
+    model = Post
     queryset = Post.objects.filter(status=1)
-    template = "post_list.html"
